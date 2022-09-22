4a4df671a8c650c7b1b68270026be38601bb559a
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2022-09-22 13:12:00 +0530

    basic setup

---
+++ src/components/LiveChatButton/LiveChatButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './LiveChatButton.module.css'
+const LiveChatButton=()=> {
+  return (
+    <div className={styles.floatBtn}> Btn</div>
+  )
+}
+
+export default LiveChatButton
