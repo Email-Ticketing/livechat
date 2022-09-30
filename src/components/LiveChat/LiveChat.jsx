26029483358cec5686ee73348275bf29b169f8ea
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2022-09-30 16:51:48 +0530

    change build

--- src/components/LiveChat/LiveChat.jsx
+++ src/components/LiveChat/LiveChat.jsx
@@ -3,13 +3,11 @@ import { BsFillChatFill } from "react-icons/bs";
 import useSocketForLiveChat from "../../data-access/useSocketForLiveChat";
 import Chatbox from "./components/Chatbox/Chatbox";
 import styles from "./LiveChat.module.css";
+import {v4 as uuid} from 'uuid'
 const LiveChat = ({teamCdn}) => {
   const [isBoxOpen, setIsBoxOpen] = useState(false);
   const [isLoggedIn,setIsLoggedIn]=useState(false)
-  const [inputDetails,setInputDetails]=useState({
-    username:'',
-    room:''
-  })
+  const [username,setUsername]=useState()
   const [latestActivityFromSocket,setLatestActivityFromSocket]=useState()
   // const [isButtonClicked,setIsButtonClicked]=useState(false)
   const [socket]=useSocketForLiveChat(setLatestActivityFromSocket)
@@ -17,12 +15,9 @@ const LiveChat = ({teamCdn}) => {
 
   const joinClickHandler=async()=>{
     console.log(teamCdn)
-    await socket.current.emit('join-chat',inputDetails.username,inputDetails.room,teamCdn)
+    await socket.current.emit('join-chat',username,uuid(),'JntyzZCjqwzkQNxjaTCU2')
     setIsBoxOpen(false)
     setIsLoggedIn(true)
-  }
-  const setDetails=(e)=>{
-    setInputDetails(details=>({...details,[e.target.name]:e.target.value}))
   }
   useEffect(()=>{
     if(latestActivityFromSocket){
@@ -34,10 +29,7 @@ const LiveChat = ({teamCdn}) => {
     <div className={styles.liveChatContainer}>
       <div className={styles.floatBtn} onClick={() => {
         setIsBoxOpen(!isBoxOpen)
-        setInputDetails({
-          username:'',
-          room:''
-        })
+        setUsername('')
         setIsLoggedIn(false)
         socket.current.emit('leave-room')
       }}>
@@ -51,18 +43,14 @@ const LiveChat = ({teamCdn}) => {
           <main>
             <div className={styles.inputField}>
               <label htmlFor="">Username:</label>
-              <input type="text" name="username" value={inputDetails.username} onChange={setDetails}/>
-            </div>
-            <div className={styles.inputField}>
-              <label htmlFor="">Room name:</label>
-              <input type="text" name="room" value={inputDetails.room} onChange={setDetails}/>
+              <input type="text" name="username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
             </div>
           </main>
           <button className={styles.joinBtn} onClick={joinClickHandler}>Join room</button>
         </div>
       )}
       {
-        isLoggedIn && <Chatbox socket={socket} allMessages={msgList} username={inputDetails.username} />
+        isLoggedIn && <Chatbox socket={socket} allMessages={msgList} username={username} />
       }
     </div>
   );
