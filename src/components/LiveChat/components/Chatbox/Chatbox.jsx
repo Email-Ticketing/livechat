2db9fc72207f162922abe2967aec41e33648ea42
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2022-10-13 13:33:29 +0530

    feat:new UI

--- src/components/LiveChat/components/Chatbox/Chatbox.jsx
+++ src/components/LiveChat/components/Chatbox/Chatbox.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import styles from "./Chatbox.module.css";
+import styles from "./Chatbox.module.scss";
 import { RiSendPlane2Fill } from "react-icons/ri";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import { useEffect } from "react";
 import useSocketForStream from "../../../../data-access/useSocketForStream";
 import { v4 as uuid } from "uuid";
 import { usePeer } from "../../../../context/PeerContext";
-import {BsFillCameraVideoFill} from 'react-icons/bs'
+import { BsFillCameraVideoFill } from "react-icons/bs";
+import { Attachment, ChatSupport, Send } from "../../../../libs/icons/icon";
+import moment from "moment/moment";
 const addToCall = (user, myPeer, myStream) => {
   const call = myPeer.call(user.user_id, myStream);
 };
-const Chatbox = ({ socket, allMessages, username }) => {
+const Chatbox = ({ socket, allMessages, username, teamCdn }) => {
   const [inputMsg, setInputMsg] = useState("");
   const [myStream, setMyStream] = useState();
   const [latestActivityFromStreamSocket, setLatestActivityFromStreamSocket] =
@@ -37,7 +39,12 @@ const Chatbox = ({ socket, allMessages, username }) => {
     });
   });
   const clickHandler = async () => {
-    await socket.current.emit("chat-message", inputMsg);
+    await socket.current.emit(
+      "chat-message",
+      inputMsg,
+      "customer",
+      "fuFCL6RKOoWw0nuLPTGTn"
+    );
     setInputMsg("");
   };
   const vidClickHandler = async () => {
@@ -53,18 +60,27 @@ const Chatbox = ({ socket, allMessages, username }) => {
   return (
     <div className={styles.chatBox}>
       <header>
-        <h2>Live chat</h2>
-        <div onClick={vidClickHandler} className={styles.vidBtn}><BsFillCameraVideoFill color='white' size={25}/></div>
+        <div className={styles.chatHeader}>
+          {" "}
+          <ChatSupport /> Live chat
+        </div>
       </header>
       <main>
         {allMessages.map((msg) => {
           return (
-            <div
-              className={
-                styles.msg + " " + (username === msg.username && styles.userMsg)
-              }
-            >
-              {msg.message}
+            <div className={styles.msgContainerLeft+ " "+ (username === msg?.user?.username && styles.msgContainerRight)}>
+              <div
+                className={
+                  styles.msg +
+                  " " +
+                  (username === msg?.user?.username && styles.userMsg)
+                }
+              >
+                {msg.message}
+              </div>
+              <p className={styles.msgInfo}>
+              {moment(msg?.created_at).format("Do MMMM YYYY, h:mm a")}
+              </p>
             </div>
           );
         })}
@@ -78,11 +94,13 @@ const Chatbox = ({ socket, allMessages, username }) => {
         <div className={styles.sendMessage}>
           <input
             type="text"
+            placeholder="Write here ..."
             value={inputMsg}
             onChange={(e) => setInputMsg(e.target.value)}
           />
-          <div onClick={clickHandler}>
-            <RiSendPlane2Fill size={20} className={styles.icon} />
+          <div onClick={clickHandler} className={styles.sendOptions}>
+            <Attachment className={styles.icon} />
+            <Send className={styles.icon} />
           </div>
         </div>
       </footer>
---
+++ src/libs/icons/assets/icons/icon-map.js
@@ -0,0 +1,9 @@
+import {ReactComponent as ChatSupport} from './chat-support.svg'
+import {ReactComponent as Send} from './send.svg'
+import {ReactComponent as Attachment} from './attachment.svg'
+
+export default {
+    chat_support:ChatSupport,
+    send:Send,
+    attachment:Attachment
+}
