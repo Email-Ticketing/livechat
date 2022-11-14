eab62f8d6c1d9636e357ac7a1b3e6a3de4b91cdd
tirth0 <tirtharajsengupta@gmail.com>
2022-11-14 12:44:43 +0530

    yaml files fixes

--- livechat-cdn.sh
+++ livechat-cdn.sh
@@ -5,17 +5,26 @@ do
     esac
 done
 
+echo "Syncing livechat repo on branch $branch"
+
+cd livechat
+
 git checkout $branch
 
 git fetch --all
 git reset --hard origin/$branch
 
+npm install
+
+echo "Disabling hashes in webpack config"
 
 # Replace hash from JS
 sed -i 's#static/js/[name].[contenthash:8].chunk.js#static/js/[name].chunk.js#g' ./node_modules/react_scripts/config/webpack.config.js
 
 # Replace hash from CSS
 sed -i 's#static/css/[name].[contenthash:8].css#static/js/[name].chunk.js#g' ./node_modules/react_scripts/config/webpack.config.js
+
+echo "Building livechat on branch $branch"
 
 npm run build
 
