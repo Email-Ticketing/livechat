47a0298fa34acae20189be4d6442512726ee2c49
divya <divyanamdev8795@gmail.com>
2022-12-29 12:24:34 +0530

    bug-fix:attachment bug fix

--- src/components/LiveChat/components/Chatbox/Chatbox.jsx
+++ src/components/LiveChat/components/Chatbox/Chatbox.jsx
@@ -1,167 +1,205 @@
-import React, { useState } from "react"
-import styles from "./Chatbox.module.scss"
+import React, { useState } from "react";
+import styles from "./Chatbox.module.scss";
 // import { RiSendPlane2Fill } from "react-icons/ri"
-import VideoPlayer from "./VideoPlayer/VideoPlayer"
-import { useEffect } from "react"
-import useSocketForStream from "../../../../data-access/useSocketForStream"
-import { v4 as uuid } from "uuid"
-import { usePeer } from "../../../../context/PeerContext"
+import VideoPlayer from "./VideoPlayer/VideoPlayer";
+import { useEffect } from "react";
+import useSocketForStream from "../../../../data-access/useSocketForStream";
+import { v4 as uuid } from "uuid";
+import { usePeer } from "../../../../context/PeerContext";
 // import { BsFillCameraVideoFill } from "react-icons/bs"
-import { Attachment, ChatSupport, Download, ImageFile, Send } from "../../../../libs/icons/icon"
-import moment from "moment/moment"
-import { useCookies } from "react-cookie"
-import stripHTML from "../../../../libs/utils/stripHtml"
-import useChat from "../../../../data-access/useChat"
-import Spinner from "../../../../libs/utils/Spinner/Spinner"
+import {
+  Attachment,
+  ChatSupport,
+  Download,
+  ImageFile,
+  Send,
+  Delete,
+} from "../../../../libs/icons/icon";
+import moment from "moment/moment";
+import { useCookies } from "react-cookie";
+import stripHTML from "../../../../libs/utils/stripHtml";
+import useChat from "../../../../data-access/useChat";
+import Spinner from "../../../../libs/utils/Spinner/Spinner";
 // import VoiceMemos from "./components/VoiceMemos/VoiceMemos"
-import { MdScreenShare } from "react-icons/md"
-import html2canvas from "html2canvas"
-import { useRef } from "react"
-import useAutosizeTextArea from "./components/AutoSizeTextArea/AutoSizeTextArea"
-import MessageContent from "./components/MessageContent/MessageContent"
+import { MdScreenShare } from "react-icons/md";
+import html2canvas from "html2canvas";
+import { useRef } from "react";
+import useAutosizeTextArea from "./components/AutoSizeTextArea/AutoSizeTextArea";
+import MessageContent from "./components/MessageContent/MessageContent";
 
 const addToCall = (user, myPeer, myStream) => {
-  const call = myPeer.call(user.user_id, myStream)
-}
+  const call = myPeer.call(user.user_id, myStream);
+};
 const Chatbox = ({ socket, allMessages, teamCdn, setIsBoxOpen }) => {
-  const endRef = useRef()
-  const textAreaRef = useRef(null)
+  const endRef = useRef();
+  const textAreaRef = useRef(null);
 
-  const [cookies, setCookies] = useCookies(["chat_room_id", "chat_session_id", "chat_user_id", "support_chat_id"])
+  const [cookies, setCookies] = useCookies([
+    "chat_room_id",
+    "chat_session_id",
+    "chat_user_id",
+    "support_chat_id",
+  ]);
 
-  const { uploadMultimediaApi } = useChat()
+  const { uploadMultimediaApi } = useChat();
 
-  const [inputMsg, setInputMsg] = useState("")
-  const [myStream, setMyStream] = useState()
-  const [files, setFiles] = useState([])
-  const [uploadingMultimedia, setUploadingMultimedia] = useState(false)
+  const [inputMsg, setInputMsg] = useState("");
+  const [myStream, setMyStream] = useState();
+  const [files, setFiles] = useState([]);
+  const [image, setImage] = useState();
+  const [uploadingMultimedia, setUploadingMultimedia] = useState(false);
   // const [file, setFile] = useState()
-  const [supportMsgId, setSupportMsgId] = useState()
-  const [isTakingSnapshot, setIsTakingSnapshot] = useState(false)
-  const [latestActivityFromStreamSocket, setLatestActivityFromStreamSocket] = useState()
-  const { peerState, setPeerState } = usePeer()
+  const [supportMsgId, setSupportMsgId] = useState();
+  const [isTakingSnapshot, setIsTakingSnapshot] = useState(false);
+  const [latestActivityFromStreamSocket, setLatestActivityFromStreamSocket] =
+    useState();
+  const { peerState, setPeerState } = usePeer();
 
-  useAutosizeTextArea(textAreaRef.current, inputMsg)
+  useAutosizeTextArea(textAreaRef.current, inputMsg);
 
   useEffect(() => {
     peerState?.myPeer.on("open", (id) => {
-      console.log("My id:", id)
-    })
-  })
+      console.log("My id:", id);
+    });
+  });
   const clickHandler = async () => {
     // console.log("teamChatCdn", teamCdn)
-    const numberOfLineBreaks = (inputMsg.match(/\n/g) || []).length
-    const inputMsgLength = inputMsg.length
-    if ((inputMsg && inputMsgLength > numberOfLineBreaks) || files?.length > 0) {
-      await socket.current.emit("chat-message", inputMsg, "customer", cookies.chat_room_id, cookies.chat_session_id, teamCdn, supportMsgId)
-      setInputMsg("")
-      setFiles([])
-      setSupportMsgId()
+    const numberOfLineBreaks = (inputMsg.match(/\n/g) || []).length;
+    const inputMsgLength = inputMsg.length;
+    if (
+      (inputMsg && inputMsgLength > numberOfLineBreaks) ||
+      files?.length > 0
+    ) {
+      await socket.current.emit(
+        "chat-message",
+        inputMsg,
+        "customer",
+        cookies.chat_room_id,
+        cookies.chat_session_id,
+        "b-j0dIGFE-WjbCM7aWyTw",
+        supportMsgId
+      );
+      setInputMsg("");
+      setFiles([]);
+      setSupportMsgId();
     }
-  }
+  };
   const vidClickHandler = async () => {
-    const stream = await navigator.mediaDevices.getDisplayMedia()
-    const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true })
-    const audioTrack = await audioStream.getAudioTracks()[0]
-    await stream.addTrack(audioTrack)
-    setMyStream(stream)
+    const stream = await navigator.mediaDevices.getDisplayMedia();
+    const audioStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+    const audioTrack = await audioStream.getAudioTracks()[0];
+    await stream.addTrack(audioTrack);
+    setMyStream(stream);
     if (peerState?.user) {
-      const call = peerState.myPeer.call(peerState.user.user_id, stream)
+      const call = peerState.myPeer.call(peerState.user.user_id, stream);
       call.on("stream", (mediaStream) => {
-        console.log("incomingStream")
-        console.log(mediaStream)
+        console.log("incomingStream");
+        console.log(mediaStream);
         setPeerState((state) => ({
           ...state,
           remoteAudioStream: mediaStream,
-        }))
-      })
+        }));
+      });
     }
-  }
+  };
 
   useEffect(() => {
     if (files?.length > 0) {
-      setUploadingMultimedia(true)
+      setUploadingMultimedia(true);
 
-      const formData = new FormData()
+      const formData = new FormData();
 
       for (let i = 0; i < files?.length; i++) {
-        formData.append(`attachment`, files[i])
+        formData.append(`attachment`, files[i]);
       }
 
-      formData.append("support_chat_id", cookies?.support_chat_id)
-      formData.append("msg_type", "customer")
-      formData.append("chat_user_id", cookies?.chat_user_id)
+      formData.append("support_chat_id", cookies?.support_chat_id);
+      formData.append("msg_type", "customer");
+      formData.append("chat_user_id", cookies?.chat_user_id);
 
       try {
         uploadMultimediaApi(formData).then((data) => {
-          setSupportMsgId(data?.data?.support_message_id)
-          setUploadingMultimedia(false)
-          setIsTakingSnapshot(false)
-        })
+          setSupportMsgId(data?.data?.support_message_id);
+          setUploadingMultimedia(false);
+          setIsTakingSnapshot(false);
+        });
       } catch (err) {
-        console.log(err)
-        setUploadingMultimedia(false)
-        setIsTakingSnapshot(false)
+        console.log(err);
+        setUploadingMultimedia(false);
+        setIsTakingSnapshot(false);
       }
     }
-  }, [files])
+  }, [files]);
 
   function formatBytes(bytes) {
-    var sizes = ["Bytes", "KB", "MB", "GB", "TB"]
-    if (bytes == 0) return "0 Byte"
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-    return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i]
+    var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    if (bytes == 0) return "0 Byte";
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
   }
 
   const handleSnapshot = () => {
-    if (isTakingSnapshot) return
+    if (isTakingSnapshot) return;
 
-    setIsTakingSnapshot(true)
-    const root = document.body
+    setIsTakingSnapshot(true);
+    const root = document.body;
 
     html2canvas(root, {
       cacheBust: true,
+      useCORS: true,
+      letterRendering: 1,
+      allowTaint: true,
       ignoreElements: function (element) {
         /* Remove element with id="live-chat" */
         if ("live-chat" === element.id) {
-          return true
+          return true;
         }
       },
+      onrendered: function (canvas) {},
     })
       .then((canvas) => {
-        const dataUrl = canvas.toDataURL("image/png")
-        var file
+        const dataUrl = canvas.toDataURL("image/png");
+        var file;
         canvas.toBlob((blob) => {
-          file = new File([blob], "fileName.jpg", { type: "image/jpeg" })
-          setFiles((prev) => [...prev, file])
-        }, "image/jpeg")
+          file = new File([blob], "screenshot.jpeg", { type: "image/jpeg" });
+          setFiles((prev) => [...prev, file]);
+        }, "image/jpeg");
+        setImage(dataUrl);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
+  const screenshotDownloadHandler = () => {
+    const a = document.createElement("a");
+    a.setAttribute("download", "screenshot.jpeg");
+    a.setAttribute("href", image);
+    a.click();
+  };
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
-      clickHandler()
-      e.preventDefault()
+      clickHandler();
+      e.preventDefault();
     }
-  }
+  };
 
   useEffect(() => {
-    endRef.current?.scrollIntoView({ behaviour: "smooth", block: "end" })
+    endRef.current?.scrollIntoView({ behaviour: "smooth", block: "end" });
 
     const timer = setTimeout(() => {
-      endRef.current?.scrollIntoView({ behaviour: "smooth", block: "end" })
-    }, 500)
-    return () => clearTimeout(timer)
-  })
+      endRef.current?.scrollIntoView({ behaviour: "smooth", block: "end" });
+    }, 500);
+    return () => clearTimeout(timer);
+  });
 
   return (
     <div className={styles.chatBox}>
       <header
         onClick={() => {
-          setIsBoxOpen(false)
+          setIsBoxOpen(false);
         }}
       >
         <div className={styles.chatHeader}>
@@ -173,19 +211,43 @@ const Chatbox = ({ socket, allMessages, teamCdn, setIsBoxOpen }) => {
         {allMessages.map((msg) => {
           return (
             (msg?.content || msg?.Support_Chat_Attachments?.length > 0) && (
-              <div className={styles.msgContainerLeft + " " + (cookies.chat_user_id === msg.user?.chat_user_id && styles.msgContainerRight)}>
-                <div className={styles.msg + " " + ("customer" === msg?.user_type && styles.userMsg)}>
+              <div
+                className={
+                  styles.msgContainerLeft +
+                  " " +
+                  (cookies.chat_user_id === msg.user?.chat_user_id &&
+                    styles.msgContainerRight)
+                }
+              >
+                <div
+                  className={
+                    styles.msg +
+                    " " +
+                    ("customer" === msg?.user_type && styles.userMsg)
+                  }
+                >
                   {" "}
-                  <div className={styles.text}>{<MessageContent msg={msg} />}</div>
+                  <div className={styles.text}>
+                    {<MessageContent msg={msg} />}
+                  </div>
                   {msg?.Support_Chat_Attachments?.length > 0 && (
                     <div className={styles.images}>
                       {msg?.Support_Chat_Attachments?.map((attachment) => {
                         return (
                           <div className={styles.image}>
                             {attachment?.attachment_type === "audio/wav" ? (
-                              <audio controls id="audio" src={attachment?.attachment_url} type={attachment?.attachment_type}></audio>
+                              <audio
+                                controls
+                                id="audio"
+                                src={attachment?.attachment_url}
+                                type={attachment?.attachment_type}
+                              ></audio>
                             ) : attachment.attachment_type.includes("image") ? (
-                              <a href={attachment?.attachment_url} className={styles.download_link} download>
+                              <a
+                                onClick={screenshotDownloadHandler}
+                                href={attachment?.attachment_url}
+                                className={styles.download_link}
+                              >
                                 <img src={attachment?.attachment_url} alt="#" />
                               </a>
                             ) : (
@@ -198,28 +260,35 @@ const Chatbox = ({ socket, allMessages, teamCdn, setIsBoxOpen }) => {
                                 </div>
                                 <div className={styles.attachemntText}>
                                   <h5>{attachment.attachment_title}</h5>
-                                  <p>{formatBytes(attachment.attachment_size)}</p>
+                                  <p>
+                                    {formatBytes(attachment.attachment_size)}
+                                  </p>
                                 </div>
                                 {/* {fetchingFile ===
                     attachment.parsed_attachment_id ? (
                       <Spinner className={styles.spinner} />
                     ) : ( */}
 
-                                <a href={attachment?.attachment_url} className={styles.download_link}>
+                                <a
+                                  href={attachment?.attachment_url}
+                                  className={styles.download_link}
+                                >
                                   <Download className={styles.download_icon} />
                                 </a>
                               </div>
                             )}
                           </div>
-                        )
+                        );
                       })}
                     </div>
                   )}
                 </div>
-                <p className={styles.msgInfo}>{moment(msg?.created_at).format("Do MMMM YYYY, h:mm a")}</p>
+                <p className={styles.msgInfo}>
+                  {moment(msg?.created_at).format("Do MMMM YYYY, h:mm a")}
+                </p>
               </div>
             )
-          )
+          );
         })}
         <div ref={endRef} className={styles.emptyDiv}></div>
 
@@ -231,29 +300,38 @@ const Chatbox = ({ socket, allMessages, teamCdn, setIsBoxOpen }) => {
           //
         </div> */}
       </main>
-      <div className={styles.attachment_name}>
-        {files?.length > 0 &&
-          (uploadingMultimedia ? (
-            <div className={styles.loading}>
-              <Spinner />
-            </div>
-          ) : (
-            supportMsgId && <div className={styles.images_name}>{files[0].name}</div>
-          ))}
-      </div>
-
       <footer>
         <div className={styles.sendMessage}>
-          <textarea className={styles.inputMsgBox} type="text" placeholder="Write here ..." value={inputMsg} ref={textAreaRef} onChange={(e) => setInputMsg(e.target.value)} onKeyDown={(e) => handleKeyPress(e)} />
+          <textarea
+            className={styles.inputMsgBox}
+            type="text"
+            placeholder="Write here ..."
+            value={inputMsg}
+            ref={textAreaRef}
+            onChange={(e) => setInputMsg(e.target.value)}
+            onKeyDown={(e) => handleKeyPress(e)}
+          />
           <div className={styles.sendOptions}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIcZBZPttoh360vK7HP3n9PLQpL_q_YHKUhQ&usqp=CAU" alt="#" className={styles.snapshot + " " + (isTakingSnapshot && styles.blur)} onClick={handleSnapshot} />
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIcZBZPttoh360vK7HP3n9PLQpL_q_YHKUhQ&usqp=CAU"
+              alt="#"
+              className={
+                styles.snapshot + " " + (isTakingSnapshot && styles.blur)
+              }
+              onClick={handleSnapshot}
+            />
 
             {/* <VoiceMemos setFiles={setFiles} /> */}
             <div className={styles.attachments}>
               <label htmlFor="attachment">
                 <Attachment className={styles.icon} />
               </label>
-              <input type="file" name="attachment" id="attachment" onChange={(event) => setFiles(event.target.files)} />
+              <input
+                type="file"
+                name="attachment"
+                id="attachment"
+                onChange={(event) => setFiles(event.target.files)}
+              />
             </div>
             <div onClick={vidClickHandler}>
               <MdScreenShare size={25} className={styles.icon} />
@@ -262,8 +340,24 @@ const Chatbox = ({ socket, allMessages, teamCdn, setIsBoxOpen }) => {
           </div>
         </div>
       </footer>
+      <div className={styles.attachment_name}>
+        {files?.length > 0 &&
+          (uploadingMultimedia ? (
+            <div className={styles.loading}>
+              <Spinner />
+            </div>
+          ) : (
+            supportMsgId && (
+              <>
+                <div className={styles.images_name}>
+                  {files[0].name} <Delete className={styles.close_icon} />
+                </div>
+              </>
+            )
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chatbox
+export default Chatbox;
--- src/libs/icons/assets/icons/icon-map.js
+++ src/libs/icons/assets/icons/icon-map.js
@@ -1,13 +1,15 @@
-import { ReactComponent as ChatSupport } from "./chat-support.svg"
-import { ReactComponent as Send } from "./send.svg"
-import { ReactComponent as Attachment } from "./attachment.svg"
-import { ReactComponent as ImageFile } from "./image_file.svg"
-import { ReactComponent as Download } from "./download.svg"
+import { ReactComponent as ChatSupport } from "./chat-support.svg";
+import { ReactComponent as Send } from "./send.svg";
+import { ReactComponent as Attachment } from "./attachment.svg";
+import { ReactComponent as ImageFile } from "./image_file.svg";
+import { ReactComponent as Download } from "./download.svg";
+import { ReactComponent as Delete } from "./delete.svg";
 
 export default {
   image_file: ImageFile,
   download: Download,
+  delete: Delete,
   chat_support: ChatSupport,
   send: Send,
   attachment: Attachment,
-}
+};
