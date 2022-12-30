efc49031e6a6d555ce7b335cfd08e24e19fb7db1
Rishav-264 <rishavgooptu1234@gmail.com>
2022-12-30 17:34:25 +0530

    staging api name change

--- src/data-access/useSocketForLiveChat.js
+++ src/data-access/useSocketForLiveChat.js
@@ -3,7 +3,7 @@ import { usePeer } from "../context/PeerContext"
 import useSocket from "./useSocket"
 
 const useSocketForLiveChat = (setLatestActivityFromSocket) => {
-  const [socket] = useSocket("https://et-dev-api.ringover-crm.xyz/")
+  const [socket] = useSocket("https://et-staging-api.ringover-crm.xyz/")
   const { peerState, setPeerState } = usePeer()
   useEffect(() => {
     defineEvents()
