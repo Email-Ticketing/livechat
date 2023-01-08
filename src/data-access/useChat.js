aa30dce2d8c72245a550cab8d475f44143b8fb8e
Ritesh Kumar <singlaritesh007@gmail.com>
2023-01-08 19:22:39 +0530

    feat: attachment as a first msg

--- src/data-access/useChat.js
+++ src/data-access/useChat.js
@@ -1,43 +1,30 @@
-import axios from "axios";
-// import { useMutation } from "react-query"
+import axios from "axios"
+import { useMutation } from "react-query"
 const useChat = () => {
   const PublicApi = axios.create({
     baseURL: "https://et-dev-api.ringover-crm.xyz/",
     headers: {
       "Content-Type": "application/json",
     },
-  });
+  })
   // "Content-Type": "multipart/form-data",
   const uploadMultimediaApi = async (formData) => {
-    for (let key of formData.entries()) {
-      console.log(key[0]);
-      console.log(key[1]);
-    }
+    return PublicApi.patch(`v1/ticket/upload`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }).then((res) => {
+      console.log("uploadRes", res)
+      return res.data
+    })
+  }
 
-    // var object = {}
-    // for (let key of formData.entries()) {
-    //   object[key[0]] = key[1]
-    // }
-    // var json = JSON.stringify(object)
-
-    return axios
-      .patch(`https://et-dev-api.ringover-crm.xyz/v1/ticket/upload`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-
-      .then((res) => {
-        console.log("uploadRess", res);
-        return res.data;
-      });
-  };
-
-  // const { isLoading: isMultimediaUploading, mutate: uploadMultimedia } = useMutation(uploadMultimediaApi)
+  const { isLoading: isMultimediaUploading, mutate: uploadMultimedia } = useMutation(uploadMultimediaApi)
 
   return {
-    uploadMultimediaApi,
-  };
-};
+    uploadMultimedia,
+    isMultimediaUploading,
+  }
+}
 
-export default useChat;
+export default useChat
--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-let div = document.createElement("div");
-div.id = "live-chat";
-document.body.appendChild(div);
-const root = ReactDOM.createRoot(document.getElementById("live-chat"));
-const teamCdn = document.currentScript.getAttribute("cdnId");
+let div = document.createElement("div")
+div.id = "live-chat"
+document.body.appendChild(div)
+const root = ReactDOM.createRoot(document.getElementById("live-chat"))
+// const teamCdn = document.currentScript.getAttribute("cdnId");
+const teamCdn = "Gu5ezMosoEz52JkcW1zb0"
 root.render(
   // <React.StrictMode>
   <App teamCdn={teamCdn} />
   // </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
